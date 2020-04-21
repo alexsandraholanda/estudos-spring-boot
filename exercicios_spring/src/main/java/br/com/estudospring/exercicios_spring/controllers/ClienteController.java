deaf8056fdde3e1fa1f9b4e7e1b9927e4ca64cc6
unknown <alexsandraale8795@gmail.com>
2020-04-20 22:39:51 -0300

    Retornando Objeto

---
+++ exercicios_spring/src/main/java/br/com/estudospring/exercicios_spring/controllers/ClienteController.java
@@ -0,0 +1,17 @@
+package br.com.estudospring.exercicios_spring.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.estudospring.exercicios_spring.models.Cliente;
+
+@RestController
+@RequestMapping(path = "/clientes")
+public class ClienteController {
+	
+	@GetMapping(path = "/qualquer")
+	public Cliente obterCliente() {
+		return new Cliente(1, "Alexsandra", "123.123.123-78");
+	}
+}
