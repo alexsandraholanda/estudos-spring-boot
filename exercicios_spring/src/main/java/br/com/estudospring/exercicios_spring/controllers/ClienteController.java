00cdd526df1dd0b0cadd03d996a34fcdd4229d87
alexsandraholanda <alexandraale8795@gmail.com>
2020-04-24 22:49:12 -0300

    Passando parametro para web

--- exercicios_spring/src/main/java/br/com/estudospring/exercicios_spring/controllers/ClienteController.java
+++ exercicios_spring/src/main/java/br/com/estudospring/exercicios_spring/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.estudospring.exercicios_spring.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudospring.exercicios_spring.models.Cliente;
@@ -14,4 +16,15 @@ public class ClienteController {
 	public Cliente obterCliente() {
 		return new Cliente(1, "Alexsandra", "123.123.123-78");
 	}
+	
+	@GetMapping(path = "/{id}")
+	public Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "987.654.321-00");
+	}
+	
+	@GetMapping
+	public Cliente obterClientePorId2(@RequestParam (name = "id", defaultValue = "10") int id) {
+		return new Cliente(id, "Antonio", "147.258.369-00");
+	}
+	
 }
